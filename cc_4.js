9fc03f71a319093704d144500b186bc7f67511fd
fabianarequena <fabianarequena@usf.edu>
2025-02-01 22:42:45 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,3 +6,14 @@ if (purchaseAmount > 100) {
     purchaseAmount -= discount;
 }
 console.log(`Final amount after discount: $${purchaseAmount}`);
+
+
+// Task 2: Sales Report
+
+let sales = [1200, 500, 850, 1600, 2100];
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+console.log(`Total sales: $${totalSales}`);
+
