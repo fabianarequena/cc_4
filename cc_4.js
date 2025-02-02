780f338eb408b0be5a14d2c2b0c92dc3831126cc
fabianarequena <fabianarequena@usf.edu>
2025-02-01 22:48:07 -0500

    Task 3 - Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -14,6 +14,14 @@ let sales = [1200, 500, 850, 1600, 2100];
 let totalSales = 0;
 for (let i = 0; i < sales.length; i++) {
     totalSales += sales[i];
-}
+} // Created a for statment for sales to calculate the sum if they all sell
 console.log(`Total sales: $${totalSales}`);
+
+
+// Task 3: Inventory Depletion
+
+for (let stock = 10; stock > 0; stock--) {
+    console.log(`Stock remaining: ${stock}`);
+} // Created a decreasing for statement, 10,9,8, etc. in descending order until the intventory is out
+console.log("Stock is empty.");
 
